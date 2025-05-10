e5a595b3431ef0ccd0b4a090053fdd2e16e4dd96
LEE <sw09112@yonsei.ac.kr>
2025-05-10 17:56:17 +0900

    scroll

--- project/src/pages/Home.jsx
+++ project/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import BuskingCard from "../components/BuskingCard";
+import ScrollCarousel from "../components/ScrollCarousel";
 import MapBox from "../components/Mapbox";
 
 const Home = () => {
@@ -6,25 +6,23 @@ const Home = () => {
     <div className="w-full h-[71px] bg-[#3160D8] flex-shrink-0" />
   );
 
-
   return (
     <div className="flex flex-col min-h-screen bg-white">
       {/* 상단 파란 영역 */}
       <TopBanner />
 
-      {/* 3) 메인 콘텐츠: 동일한 좌우 패딩(px-4) */}
-      <main className="w-full max-w-4xl mx-auto px-4 py-6 flex flex-col gap-10"/>
-        {/* “최신 버스킹” 헤더에도 px-4 가 적용된 상태 */}
-        <h2 className="text-[32px] font-bold px-4 pt-4 -mt-8">최신 버스킹</h2>
-        {/* 밑에 회색 실선 */}
-        <hr className="border-t-2 border-gray-200 mb-6 mx-4" />
-      <BuskingCard/>
-      <MapBox/>
+      {/* 메인 콘텐츠 */}
+      <main className="w-full max-w-4xl mx-auto px-4 py-6 flex flex-col gap-10">
+        {/* 섹션 제목 */}
+        <h2 className="text-[32px] font-bold pt-4 -mt-8">최신 버스킹</h2>
 
-      
-        
-        
-      
+        {/* 구분선 */}
+        <hr className="border-t-2 border-gray-200 mb-6" />
+
+        {/* 콘텐츠 */}
+        <ScrollCarousel/>
+        <MapBox />
+      </main>
     </div>
   );
 };
